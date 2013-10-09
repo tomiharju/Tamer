ac597074a10578049908d083d288bc9fe36f2b3c
Tomi Harju <tharju@thebes>
2013-10-09 13:03:18 +0300

    Fixed out of bounds flying

--- tamer/src/com/me/tamer/ui/SpearButton.java
+++ tamer/src/com/me/tamer/ui/SpearButton.java
@@ -1,12 +1,8 @@
 package com.me.tamer.ui;
 
-import java.util.LinkedList;
-
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.me.tamer.gameobjects.Spear;
 import com.me.tamer.gameobjects.Tamer;
 import com.me.tamer.gameobjects.renders.UiRenderer;
