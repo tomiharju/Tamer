9791c949576d0d243885a937ff9c9d42df3cb282
Ville Sillanpaa <sillanpaa.ville@hotmail.com>
2013-11-13 14:00:02 +0200

    m

--- tamer/src/com/me/tamer/gameobjects/tamer/GryphonScream.java
+++ tamer/src/com/me/tamer/gameobjects/tamer/GryphonScream.java
@@ -53,36 +53,7 @@ public class GryphonScream extends StaticObject {
 	}
 	
 	public void debugDraw(ShapeRenderer shapeRenderer){
-		/*shapeRenderer.setProjectionMatrix(environment.getStage().getCamera().combined);
-		shapeRenderer.setColor(1, 1, 1, 0.9f);
-		
-		drawVert1.set(Helper.worldToScreen(tamerPos));
-		
-		shapeRenderer.begin(ShapeType.Circle);
-		shapeRenderer.translate(drawVert1.x, drawVert1.y, 0);
-		shapeRenderer.scale(1f, 0.5f, 1f);
-		shapeRenderer.circle(0, 0, SCREAM_CIRCLE_RADIUS,30);
-		
-		shapeRenderer.identity();
-		shapeRenderer.end();
-		
-		/*
-		drawVert1.set(IsoHelper.twoDToTileIso(screamVert1));
-		drawVert2.set(IsoHelper.twoDToTileIso(screamVert2));
-		drawVert3.set(IsoHelper.twoDToTileIso(screamVert3));
-		
-		shapeRenderer.begin(ShapeType.Line);
-		shapeRenderer.line(drawVert1.x, drawVert1.y, drawVert2.x, drawVert2.y );
-		shapeRenderer.end();
-		
-		shapeRenderer.begin(ShapeType.Line);
-		shapeRenderer.line(drawVert1.x, drawVert1.y, drawVert3.x, drawVert3.y );
-		shapeRenderer.end();
-		
-		shapeRenderer.begin(ShapeType.Line);
-		shapeRenderer.line(drawVert2.x, drawVert2.y, drawVert3.x, drawVert3.y );
-		shapeRenderer.end();
-		*/
+		//
 	}
 	
 	public void setGraphics(){
@@ -96,31 +67,20 @@ public class GryphonScream extends StaticObject {
 	public void update(float dt) {
 		if(isActive){
 			for(int i = 0 ; i < soundWaves.size() ; i++){
-				
-				soundWaves.get(i).add(screamDirection.tmp().mul(screamSpeed*dt));
-	
+				if(soundWaves.get(i).len() < 10)
+					soundWaves.get(i).add(screamDirection.tmp().mul(screamSpeed*i*dt));
 				if(soundWaves.get(i).len() > 10){
-					soundWaves.get(i).set(0,0,0);
+					if(i == soundWaves.size() - 1  ){
+						isActive = false;
+						for(int k = 0 ; k < soundWaves.size() ; k++)
+							soundWaves.get(k).set(0,0,1);
+					}
+					else
+						soundWaves.get(i).set(0,0,1);
+					
 				}
 			}
 		}
-		
-		
-			/*
-			if(soundWaves.get(i).len() < 10)
-				soundWaves.get(i).add(screamDirection.tmp().mul(screamSpeed*i*dt));
-			if(soundWaves.get(i).len() > 10){
-				if(i == soundWaves.size() - 1  ){
-					isActive = false;
-					for(int k = 0 ; k < soundWaves.size() ; k++)
-						soundWaves.get(k).set(0,0,1);
-				}
-				else
-					soundWaves.get(i).set(0,0,1);
-				
-			}
-			*/
-			
 
 	}
 	public void draw(SpriteBatch batch){
@@ -167,10 +127,6 @@ public class GryphonScream extends StaticObject {
 					EventPool.addEvent(new tEvent(worm,"doScreamEffect",0.5f,3));
 				}
 			}
-		
-			
-			
-		
 		}	
 		isOnCooldown = true;
 		EventPool.addEvent(new tEvent(this,"enable",3.14f,1));
@@ -185,46 +141,4 @@ public class GryphonScream extends StaticObject {
 	public boolean getDebug(){
 		return false;
 	}
-	
-	
-	//Scream Triangle
-	/*
-	tamerPos.set(environment.getTamer().getShadow().getPosition());
-	tamerHead.set(environment.getTamer().getHeading());
-	screamVert1.set(tamerPos);
-	screamVert2.set(tamerPos);
-	screamVert2.x += tamerHead.x * SCREAM_AREA_LENGTH - tamerHead.y * SCREAM_AREA_WIDTH;
-	screamVert2.y += tamerHead.y * SCREAM_AREA_LENGTH + tamerHead.x * SCREAM_AREA_WIDTH;
-	
-	screamVert3.set(tamerPos);
-	screamVert3.x += tamerHead.x * SCREAM_AREA_LENGTH + tamerHead.y * SCREAM_AREA_WIDTH;
-	screamVert3.y += tamerHead.y * SCREAM_AREA_LENGTH - tamerHead.x * SCREAM_AREA_WIDTH;
-	
-	ArrayList<Creature> creatures = environment.getCreatures();
-	
-	for (int i = 0; i < creatures.size(); i++){	
-		if(creatures.get(i).getClass() == Worm.class){
-			
-			Worm worm = ((Worm)creatures.get(i));
-
-			wormPos.set(worm.getHead().getPosition());		
-			wormPos1.set(wormPos.x - screamVert1.x, wormPos.y - screamVert1.y);
-			wormPos2.set(wormPos.x - screamVert2.x, wormPos.y - screamVert2.y);
-			wormPos3.set(wormPos.x - screamVert3.x, wormPos.y - screamVert3.y);
-			
-			float cross1 = wormPos1.crs(screamVert2.x - screamVert1.x, screamVert2.y - screamVert1.y);
-			float cross2 = wormPos2.crs(screamVert3.x - screamVert2.x, screamVert3.y - screamVert2.y);
-			float cross3 = wormPos3.crs(screamVert1.x - screamVert3.x, screamVert1.y - screamVert3.y);
-			
-			//Check with cross-product if WormHead is inside scream-area;
-			if( cross1 > 0 && cross2 > 0 && cross3 > 0){
-				newHeading.set(wormPos.x - tamerPos.x, wormPos.y - tamerPos.y);
-				newHeading.nor();
-				worm.setHeading(newHeading);
-			}
-		}	
-	}
-	*/
-
-	
 }
